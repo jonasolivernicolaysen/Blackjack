4da50ac0e341a91361a608ba4e72df80bc871583
Jonas.Nicolaysen <jonic6635@uib.no>
2025-09-25 12:24:37 +0200

    implemented Suit, Rank, Card, Deck and Hand classes

--- main.py
+++ main.py
@@ -1,14 +1,73 @@
-class Card:
-    pass
+from enum import Enum
+import random
 
 
-class Deck:
-    pass
+class Suit(Enum):
+    HEARTS = "♥"
+    DIAMONDS = "♦"
+    CLUBS = "♣"
+    SPADES = "♠"
 
 
-class Hand:
-    pass
+class Rank(Enum):
+    TWO = "2"
+    THREE = "3"
+    FOUR = "4"
+    FIVE = "5"
+    SIX = "6"
+    SEVEN = "7"
+    EIGHT = "8"
+    NINE = "9"
+    TEN = "T"
+    JACK = "J"
+    QUEEN = "Q"
+    KING = "K"
+    ACE = "A"
 
 
-class GameEngine:
-    pass
+class Card():
+    def __init__(self, suit: Suit,  rank: Rank,) -> None:
+        self.suit = suit
+        self.rank = rank
+    
+    @property
+    def value(self) -> tuple:
+        if self.rank in (Rank.JACK, Rank.QUEEN, Rank.KING):
+            return (10, )
+        elif self.rank == Rank.ACE:
+            return (1, 11)
+        else:
+            return (self.rank.value, )
+
+    def __repr__(self) -> str:
+        return f"{self.rank}{self.suit}"
+
+
+class Deck():
+    def __init__(self) -> None:
+        self.deck = [
+            (suit, rank) for suit in Suit for rank in Rank
+        ]
+        
+    def shuffle(self) -> None:
+        return random.shuffle(self.deck)
+    
+    def __repr__(self) -> str:
+        return ", ".join([(card[1].value + card[0].value) for card in self.deck])
+    
+
+class Hand():
+    def __init__(self) -> None:
+        self.hand = []
+
+    def current_hand_value(self):
+        return sum(self.hand)
+    
+
+class GameEngine():
+    def __init__(self) -> None:
+        pass
+
+deck = Deck()
+#deck.shuffle()
+print(deck)
